1f56df18e1c67664780d12b1c796f2b7dba4e937
bulatbulbulat <bulatbulbulat@gmail.com>
2026-03-06 11:04:15 +0700

    branch baru

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
 console.log("hello from other user")
 
-let umur = 20;
-
-if (umur < 18) {
-    console.log("gaboleh masuk!")
-} else {
-    console.log("oke")
-}
+console.log("ini branch baru yang ngubah file script js")
